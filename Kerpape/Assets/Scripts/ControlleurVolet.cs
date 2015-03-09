04d7f378e6cebe47790fc0d1858a540a54de0b8e
Arthur <arthur_970@hotmail.com>
2015-03-09 09:18:45 +0100

    controlleur volet termine, le reste arrive

--- Kerpape/Assets/Scripts/ControlleurVolet.cs
+++ Kerpape/Assets/Scripts/ControlleurVolet.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ControlleurVolet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private bool mouvement;
+	private Vector3 directionTranslation;
+	private Transform volet;
+	private Transform positionHaute;
+	private Transform positionBasse;
+	private float vitesse = 0.005f;
+
+	void Start (){
+
+		directionTranslation = Vector3.forward;
+		mouvement = true;
+		volet = transform.Find ("volet");
+		positionHaute = transform.Find ("position_haute");
+		positionBasse = transform.Find ("position_basse");
+
 	}
-	
-	// Update is called once per frame
 	void Update () {
+
+		verifieLimite ();
+
+		if (mouvement) {
+
+			volet.Translate(directionTranslation * vitesse);
+
+		}
 	
 	}
+
+	public void action(string action){ 
+		switch (action) {
+			case "monte":
+				directionTranslation = Vector3.forward;
+				mouvement = true;
+			break;
+			case "descend":
+				directionTranslation = Vector3.back;
+				mouvement = true;
+			break;
+			default:
+				mouvement = false;
+			break;
+		}
+
+	}
+	private void verifieLimite (){
+
+		if(directionTranslation == Vector3.forward){
+
+			mouvement = volet.localPosition[2] < positionHaute.localPosition[2];
+
+		}else{
+
+			mouvement = volet.localPosition[2] > positionBasse.localPosition[2]; 
+
+		}
+
+	}
+
+
 }
+
+
+
+
+
+
+
+
+
