4e62a61781dab6fed2c162ca4478623f6242e3f0
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-04-14 21:31:42 +0200

    Mouvements au clavier
    Ajout de KeyboardNavihation au game object Utilisateur qui permet de se
    déplacer au clavier mais pas de bouger la caméra

--- Kerpape/Assets/Scripts/Navigation/KeyboardNavigation.cs
+++ Kerpape/Assets/Scripts/Navigation/KeyboardNavigation.cs
@@ -4,28 +4,89 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class KeyboardNavigation : MonoBehaviour
 {
-	public bool useKeyboard = false;	//If turned to false the FicedUpdate function doesn't execute
-										//Must be turned on only when Navigation is set to none in VRManager
-
+	//If turned to false the FicedUpdate function doesn't execute
+	//Must be turned on only when Navigation is set to none in VRManager
+	public bool useKeyboard = false;	
 	public float moveSpeed = 3.0F;
 	public float rotateSpeed = 3.0F;
 
 	void FixedUpdate ()
 	{
-		// Cache the inputs.
-		float h = Input.GetAxis("Horizontal");
-		float v = Input.GetAxis("Vertical");
-		//get the character controller
-		CharacterController controller = GetComponent<CharacterController>();
+		if (useKeyboard) {
+			// Cache the inputs.
+			float h = Input.GetAxisRaw ("Horizontal");
+			float v = Input.GetAxisRaw ("Vertical");
 
-		transform.Rotate (0, h * rotateSpeed, 0);
-		Vector3 forward = transform.TransformDirection (Vector3.forward);
-		float curSpeed = moveSpeed * v;
+			//get the character controller
+			CharacterController controller = GetComponent<CharacterController> ();
 
-		controller.SimpleMove (forward * curSpeed);
+			Vector3 move = new Vector3(h, 0, v);
+			move = transform.TransformDirection(move);
+			move*=moveSpeed;
+			controller.Move(move*Time.deltaTime);
+		}
+	}
+}
 
 
-		//MovementManagement(h, v);
+
+/*[RequireComponent(typeof(CharacterController))]
+public class KeyboardNavigation : MonoBehaviour
+{
+	//If turned to false the FicedUpdate function doesn't execute
+	//Must be turned on only when Navigation is set to none in VRManager
+	public bool useKeyboard = false;	
+	
+	//If set to true, Left and Right make us strafe
+	//If false Left and Right make rotate the head
+	public bool strafe = true;
+	
+	public float moveSpeed = 3.0F;
+	public float rotateSpeed = 3.0F;
+
+	void FixedUpdate ()
+	{
+		if (useKeyboard) {
+			// Cache the inputs.
+			float h = Input.GetAxisRaw ("Horizontal");
+			float v = Input.GetAxisRaw ("Vertical");
+
+			//get the character controller
+			CharacterController controller = GetComponent<CharacterController> ();
+			
+			//no rotation, we make a translation
+			if(strafe)
+			{
+				curSpeed = moveSpeed * h;
+				//movment to the right
+				if(h>0)
+				{
+					Vector3 right = transform.TransformDirection(Vector3.right);
+					controller.SimpleMove (right * curSpeed);
+				}
+				//movment to the left
+				else
+				{
+					Vector3 left = transform.TransformDirection(Vector3.left);
+					controller.SimpleMove (left * curSpeed);
+				}
+
+			}
+			//rotation
+			else
+			{
+				transform.Rotate (0, h * rotateSpeed, 0);
+			}
+
+			//Vector3.forward is the (0,0,1) vector
+			//TransformDirection transforms forward from local space to global space (???)
+			Vector3 forward = transform.TransformDirection (Vector3.forward);
+			//calculate the speed with regard to the vertical imput
+			curSpeed = moveSpeed * v;
+			//apply corresponding amount of movment
+			controller.SimpleMove (forward * curSpeed);
+
+		}
 	}
 
-}
+}*/
--- middlevr_config_files/test.vrx
+++ middlevr_config_files/test.vrx
@@ -7,10 +7,9 @@
 	</DeviceManager>
 	<DisplayManager Fullscreen="0" AlwaysOnTop="1" WindowBorders="0" ShowMouseCursor="1" VSync="0" AntiAliasing="0" ForceHideTaskbar="0" SaveRenderTarget="0" ChangeWorldScale="0" WorldScale="1">
 		<Node3D Name="VRSystemCenterNode" Tag="VRSystemCenter" Parent="None" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.000000,0.000000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
-		<Node3D Name="HandNode" Tag="Hand" Parent="VRSystemCenterNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.400000,0.000000,1.000000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
-		<Node3D Name="HeadNode" Tag="Head" Parent="VRSystemCenterNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.000000,1.700000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
+		<Node3D Name="HeadNode" Tag="Head" Parent="VRSystemCenterNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.000000,1.200000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
 		<Camera Name="Camera0" Parent="HeadNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.000000,0.000000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" VerticalFOV="60" Near="0.1" Far="1000" Screen="0" ScreenDistance="1" UseViewportAspectRatio="1" AspectRatio="1.33333" />
-		<Node3D Name="WandNode" Parent="VRSystemCenterNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.600000,1.700000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
+		<Node3D Name="WandNode" Parent="VRSystemCenterNode" Tracker="0" IsFiltered="0" Filter="0" PositionLocal="0.000000,0.600000,1.200000" OrientationLocal="0.000000,0.000000,0.000000,1.000000" />
 		<Viewport Name="Viewport0" Left="0" Top="0" Width="1280" Height="800" Camera="Camera0" Stereo="0" StereoMode="3" CompressSideBySide="0" StereoInvertEyes="0" OculusRiftWarping="0" UseHomography="0" />
 	</DisplayManager>
 	<ClusterManager NVidiaSwapLock="0" DisableVSyncOnServer="1" ForceOpenGLConversion="0" BigBarrier="0" SimulateClusterLag="0" MultiGPUEnabled="0" ImageDistributionMaxPacketSize="8000" />
