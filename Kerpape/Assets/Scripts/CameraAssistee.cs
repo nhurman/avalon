9d2c17ed9e974f8f78c6158d02653e9820854fd5
Alexandre Audinot <aaudinot@insa-rennes.fr>
2015-05-25 02:52:45 +0200

    ajout du script CameraAssistee.cs

---
+++ Kerpape/Assets/Scripts/CameraAssistee.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAssistee : MonoBehaviour {
+	public Camera camera;
+	
+	public GameObject posShutters;
+	public GameObject posLightKitchen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void lookAtShutters()
+	{
+		camera.transform.position = posShutters.transform.position;
+		camera.transform.rotation = posShutters.transform.rotation;
+	}
+	public void lookAtKitchenLight()
+	{
+		camera.transform.position = posLightKitchen.transform.position;
+		camera.transform.rotation = posLightKitchen.transform.rotation;
+	}
+	
+	
+	[ContextMenu("Debug")]
+	private  void ResetName()
+	{
+		lookAtKitchenLight();
+	}
+}
