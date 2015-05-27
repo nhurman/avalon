594d67e2b9014e39c9adca37a3c1cf5a41f70d7b
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-05-27 13:27:15 +0200

    ajout d'un sous menu au menu

---
+++ Kerpape/Assets/Scripts/CustomMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomMenu : MonoBehaviour {
+
+	private VRMenu vrMenu			 	= null;
+
+	// Use this for initialization
+	void Start () {
+		while (vrMenu == null || vrMenu.menu == null)
+		{
+			vrMenu = FindObjectOfType(typeof(VRMenu)) as VRMenu;
+		}
+	}
+	
+	private void choixMode()
+	{
+		// Add a button at the start of the menu
+		//sousMenuMode = new vrCommand(MyItemCommandHandler);
+		
+		vrWidgetMenu sousMenuMode = new vrWidgetMenu("choixMode", vrMenu.menu, "Choix du mode");
+		vrMenu.menu.SetChildIndex(sousMenuMode, 0);
+	}
+}
