85c493f56eb1fe6972f064d69cbaa79170be6323
Julien <julien.bouvet@insa-rennes.fr>
2015-05-27 15:27:32 +0200

    collider bouton porte + vr actor || script basique tv on/off

---
+++ Kerpape/Assets/Scripts/ActionTV.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionTV : MonoBehaviour {
+
+	GameObject planTV = null;
+	bool activated	  = false;
+
+	// Use this for initialization
+	void Start () {
+		planTV = GameObject.Find ("tele_allumee");
+		planTV.transform.Translate (0, 15, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void VRAction(){
+		if (activated) {
+			planTV.transform.Translate (0, 15, 0);
+			activated = false;
+		} else {
+			planTV.transform.Translate (0, -15, 0);
+			activated = true;
+		}
+	}
+}
