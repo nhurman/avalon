a55bb7fa8159bbbcc9e92a5678a125e8d14c5e53
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-05-28 16:55:30 +0200

    VRFPSinputcontroller doc

--- Kerpape/Assets/MiddleVR_FirstPersonController/VRFPSInputController.cs
+++ Kerpape/Assets/MiddleVR_FirstPersonController/VRFPSInputController.cs
@@ -7,69 +7,61 @@ using System.Collections;
 using MiddleVR_Unity3D;
 using System;
 
-
+///<summary>
+///Handles user movments with keyboard and mouse. 
+///</summary>
 public class VRFPSInputController : MonoBehaviour
 {
-    public string ReferenceNode   			= "HandNode";
-	public bool BlockInputs 				= false;
-	public float Sensibility 				= 2.0f;
+    public string ReferenceNode   				= "HandNode";
+	public bool BlockInputs 					= false;		//set to true when the user is not allowed to move
+	public float Sensibility 					= 3.0f;			//float used to modify the input's values and thus movment speed
 
-    private bool  m_SearchedRefNode			= false;
-    private GameObject m_RefNode   			= null;
+    private bool  m_SearchedRefNode				= false;
+    private GameObject m_RefNode   				= null;
     	
-	//verticalAngle will contain the vertical orientation of the wand
-	public float verticalAngle				= 0.0f;
+	public float verticalAngle					= 0.0f;			//verticalAngle will contain the vertical orientation of the wand
 
-	private GameObject head 				= null;
-	private CharacterController controller 	= null;
-	private GameObject wand 				= null;
+	private GameObject head 					= null;			//embodies the user's head, used for camera movments 
+	private CharacterController charController 	= null;			//object attached to the user avatar "Utilisateur" that provides movment functions
+	private GameObject wand 					= null;
 
-	//keyb will reference the keyboard and be used to check if movments keys are pressed
-	private vrKeyboard keyb 				= null;
+	private vrKeyboard keyb 					= null;			//keyb will reference the keyboard and be used to check if movments keys are pressed
 
-	private WandOnlyController wController	= null;
+	private WandOnlyController wandController	= null;			//a script that controls the wand, i.e the cursor, movments when the inputs are blocked
 
-    // Use this for initialization
     void Start()
     {
 		head = GameObject.Find("HeadNode");
 		wand = GameObject.Find("VRWand");
-		controller = GetComponent<CharacterController>();
-		wController = GetComponent<WandOnlyController>();
+		charController = GetComponent<CharacterController>();
+		wandController = GetComponent<WandOnlyController>();
 		if (m_RefNode == null)
 			m_RefNode = GameObject.Find(ReferenceNode);
 	}
 
-	// Update is called once per frame
-    void Update(){
-
-
+    void Update()
+    {
 		//Affect keyb to the MiddleVR Keyboard
 		if (keyb == null)
 			keyb = MiddleVR.VRDeviceMgr.GetKeyboard();
-
-		if (keyb.IsKeyToggled(MiddleVR.VRK_R)){
-			invertBlockInput();
-		}
-
-		//if BlockInputs == true, we don't want the user to be able to move the camera by himself
-		//if(!BlockInputs){
-			lookingUpDown();
-			lookingLeftRight();
-		//}
+		lookingUpDown();
+		lookingLeftRight();
 		moving(keyb);
     }
 
+    ///<summary>
+	///Allows the user to rotate the head, embodied by HeadNode, on a vertical axis
+	///</summary>
 	void lookingUpDown()
 	{
 		if (!BlockInputs) {
-			//Get the wand vertical orientation
+			//Get the wand vertical offset
 			float wandVertical = MiddleVR.VRDeviceMgr.GetWandVerticalAxisValue ();
 
 			//If the wand orientation is not in a neutral position (i.e. poiting perfectly toward us)
 			// Modifying the last registered vertical angle according to the direction the Wand is pointing 
 			if (Math.Abs (wandVertical) > 0.1f) {
-				//Modifying verticalAngle value 
+				//Modifying verticalAngle value ; we consider that wandVertical is an angle even though it's an offset
 				verticalAngle += wandVertical;
 				//Putting it back to a threshold value in case it exceeded said threshold
 				if (verticalAngle > 60f)
@@ -86,9 +78,12 @@ public class VRFPSInputController : MonoBehaviour
 		wand.transform.Translate (0, 0, -0.5f);
 	}
 
+	///<summary>
+	///Allows the user to rotate the body on a horizontal axis
+	///</summary>
 	void lookingLeftRight()
 	{
-		//horizontalAngle will contain the horizontal orientation of the wand
+		//horizontalAngle will contain the horizontal offset of the wand
 		float horizontalAngle = 0.0f;
 		if(!BlockInputs) {
 			//Get the wand horizontal orientation
@@ -105,6 +100,9 @@ public class VRFPSInputController : MonoBehaviour
 		transform.Rotate(Vector3.up, horizontalAngle);
 	}
 
+	///<summary>
+	///Allows the user to move his avatar forward, backward, left and right. 
+	///</summary>	
 	void moving(vrKeyboard keyb)
 	{
 		//forward will be modified in case we push the up/down key of the keyboard
@@ -116,51 +114,55 @@ public class VRFPSInputController : MonoBehaviour
 		if (keyb.IsKeyPressed(MiddleVR.VRK_UP) || keyb.IsKeyPressed(MiddleVR.VRK_W))
 		{
 			forward = 1.0f;
-			//unLockCamera();
 		}
 		if (keyb.IsKeyPressed(MiddleVR.VRK_DOWN) || keyb.IsKeyPressed(MiddleVR.VRK_S))
 		{
 			forward = -1.0f;
-			//unLockCamera();
 		}
 
 		//Get MiddleVR's keyboard inputs
 		if (keyb.IsKeyPressed(MiddleVR.VRK_RIGHT) || keyb.IsKeyPressed(MiddleVR.VRK_D))
 		{
 			strafe = 1.0f;
-			//unLockCamera();
 		}
 		
 		if (keyb.IsKeyPressed(MiddleVR.VRK_LEFT) || keyb.IsKeyPressed(MiddleVR.VRK_A))
 		{
 			strafe = -1.0f;
-			//unLockCamera();
 		}
-
 		Vector3 directionVector = new Vector3(strafe, 0, forward)*Sensibility;
-		controller.SimpleMove(m_RefNode.transform.TransformDirection(directionVector));
+		charController.SimpleMove(m_RefNode.transform.TransformDirection(directionVector));
 	}
 
-
+	///<summary>
+	///Blocks all movments but allows user to move the wand (i.e. the cursor)
+	///</summary>	
 	public void lockCamera(){
 		BlockInputs = true;
-		if (wController != null) {
-			wController.UnlockWand();
+		if (wandController != null) {
+			wandController.UnlockWand();
 		}
 	}
 
+	///<summary>
+	///Unblocks movments
+	///</summary>	
 	public void unLockCamera(){
 		BlockInputs = false;
-		if (wController != null) {
-			wController.LockWand ();
+		if (wandController != null) {
+			wandController.LockWand ();
 		}
 	}
 
+	///<summary>
+	///If movments are blocked, unblocks them <br>
+	///If movments are not blocked, blocks them but allows wand movments
+	///</summary>	
 	public void invertBlockInput(){
 		BlockInputs = ! BlockInputs;
 
-		if (wController != null) {
-			wController.invertWandLock();
+		if (wandController != null) {
+			wandController.invertWandLock();
 		}
 	}
 }
