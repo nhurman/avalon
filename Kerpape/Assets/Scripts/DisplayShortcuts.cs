db63d0c616b535b71219a230dedd2cee0e18caec
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-06-14 19:32:47 +0200

    menu middlevr désactivé + commentaires + inputs bloqués qd les raccourcis sont affichés

--- Kerpape/Assets/Scripts/DisplayShortcuts.cs
+++ Kerpape/Assets/Scripts/DisplayShortcuts.cs
@@ -1,33 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Controls the appearance/disappearance a non-interactive menu that displays keyboard shortcuts
+/// </summary>
 public class DisplayShortcuts : MonoBehaviour {
-	private GameObject menu				= null;
-	private vrKeyboard keyb				= null;
-	private double timeInCurrentState	= 0.0;
+	private GameObject menu						= null;			//menu is the plane to be displayed when pressing escape
+	private vrKeyboard keyb						= null;			//MiddleVR representation of a keyboard
+	private double timeInCurrentState			= 0.0;			//number of seconds since the last time the menu state changed (from hidden to displayed or the opposite)
+	private VRFPSInputController controller		= null;			//a script that allows, among other things, to block inputs from the user
 
-	// Use this for initialization
+
 	void Start () {
 		menu = GameObject.Find("KeyBindingMenu");
-		menu.SetActive(false);
+		menu.SetActive(false);									//at the begining of the simulation, the menu is hidden
 		keyb = MiddleVR.VRDeviceMgr.GetKeyboard();
+		controller = GetComponent<VRFPSInputController>();
+
 	}
-	
+
+	/// <summary>
+	/// Checks if the menu is in its current state from more than 0.5s and, eventually, changes its state if the user is pressing escape
+	/// The user can't move when menu is displayed
+	/// </summary>
 	void Update()
 	{
 		timeInCurrentState += MiddleVR.VRKernel.GetDeltaTime();
-		Debug.Log (timeInCurrentState);
 		if(timeInCurrentState > 0.5)
 		{
 			if (keyb.IsKeyPressed(MiddleVR.VRK_ESCAPE) && menu.activeSelf == false)
 			{
 				menu.SetActive(true);
 				timeInCurrentState = 0.0;
+				controller.lockCamera();
 			}
 			else if (keyb.IsKeyPressed(MiddleVR.VRK_ESCAPE) && menu.activeSelf == true)
 			{
 				menu.SetActive(false);
 				timeInCurrentState = 0.0;
+				controller.unLockCamera();
 			}
 		}
 	}
