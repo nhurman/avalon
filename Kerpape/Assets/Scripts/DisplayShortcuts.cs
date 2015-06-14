e16f8543737ac296dffbd6c6628c9e1652134872
Alexandre Leonardi <alexandre.leonardi@free.fr>
2015-06-14 17:34:26 +0200

    touche ESCAPE affiche la liste des raccourcis claviers

---
+++ Kerpape/Assets/Scripts/DisplayShortcuts.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayShortcuts : MonoBehaviour {
+	private GameObject menu				= null;
+	private vrKeyboard keyb				= null;
+	private double timeInCurrentState	= 0.0;
+
+	// Use this for initialization
+	void Start () {
+		menu = GameObject.Find("KeyBindingMenu");
+		menu.SetActive(false);
+		keyb = MiddleVR.VRDeviceMgr.GetKeyboard();
+	}
+	
+	void Update()
+	{
+		timeInCurrentState += MiddleVR.VRKernel.GetDeltaTime();
+		Debug.Log (timeInCurrentState);
+		if(timeInCurrentState > 0.5)
+		{
+			if (keyb.IsKeyPressed(MiddleVR.VRK_ESCAPE) && menu.activeSelf == false)
+			{
+				menu.SetActive(true);
+				timeInCurrentState = 0.0;
+			}
+			else if (keyb.IsKeyPressed(MiddleVR.VRK_ESCAPE) && menu.activeSelf == true)
+			{
+				menu.SetActive(false);
+				timeInCurrentState = 0.0;
+			}
+		}
+	}
+}
